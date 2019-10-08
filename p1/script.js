57c8798d671dc885049e37da27c1c30ac44937a4
Jeff <github@314ghz.com>
2019-10-08 05:30:59 -0500

    Added some basic functionality for the Rock Paper Scissors game in Vue.js.

--- p1/script.js
+++ p1/script.js
@@ -1,6 +1,57 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		message: 'Hello Vue!'
+		message: 'Hello Vue!',
+		move: null
+	},
+	methods: {
+		submitMove: function (event) {
+			const computerMove = getComputerMove();
+			const problemMessage = 'There was a problem, please make sure you chose a valid move.'
+			if (!this.move) {
+				this.message = problemMessage;
+			} else if (this.move === computerMove) {
+				this.message = 'Tie';
+			} else {
+				switch (this.move) {
+					case 'Rock':
+						if (computerMove === 'Paper') {
+							this.message = 'Computer Won';
+						} else if (computerMove === 'Scissors') {
+							this.message = 'You Won';
+						}
+						break;
+					case 'Paper':
+						if (computerMove === 'Rock') {
+							this.message = 'You Won';
+						} else if (computerMove === 'Scissors') {
+							this.message = 'Computer Won';
+						}
+						break;
+					case 'Scissors':
+						if (computerMove === 'Rock') {
+							this.message = 'Computer Won';
+						} else if (computerMove === 'Paper') {
+							this.message = 'You Won';
+						}
+						break;
+					default:
+						this.message = problemMessage;
+						break;
+				}
+			}
+			/*
+			if (!!this.choice) {
+				this.message = "You chose " + this.choice;
+			} else {
+				this.message = "There was a problem, please make sure you chose a valid move.";
+			}
+			*/
+		}
 	}
-})
+});
+
+function getComputerMove() {
+	const moves = ['Rock', 'Paper', 'Scissors'];
+	return moves[Math.floor(Math.random() * 3)];
+}
